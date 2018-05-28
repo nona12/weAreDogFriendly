13bfc593797d9afa3403da729bfb77617d36cd23
nona12 <nuriahervella@gmail.com>
2018-05-28 16:55:04 +0200

    Creating own middleware to avoid errors with headers

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,24 @@ var api = require('./routes/hotel')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use( '/api', api)
+
+
+// middleware
+app.use((req, res, next) => {
+    //permitimos que las peticiones se puedan hacer desde cualquier sitio
+    res.header('Access-Control-Allow-Origin', '*')
+    //res.header('Access-Control-Allow-Origin', '192.168.0.11')
+    // configuramos las cabeceras que pueden llegar
+    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method')
+    // configuramos los métodos que nos pueden llegar
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+    next(); // para que se salga de esta función 
+})
+
+
+
+
+app.use('/api', api)
 
 module.exports = app
