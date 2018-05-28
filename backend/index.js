4e57f81d25c1a446be41c28dae5bb54ee0a56707
nona12 <nuriahervella@gmail.com>
2018-05-28 15:29:41 +0200

    creation of the models

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 'use strict'
-var app =require('./app') 
+var mongoose = require('mongoose');
+var app = require('./app')
 var port = process.env.PORT || 3678
-app.listen(port, ()=>{
-console.log("API REST funcionando en http://localhost:${port}");
+
+mongoose.connect('mongodb://localhost:27017/hotels', (err, res) => {
+
+    if (err) {
+        throw err;
+    } else {
+        console.log('Conexión a mongodb correcta.')
+        app.listen(port, () => {
+            console.log("API REST funcionando en http://localhost:" + port);
+        });
+    }
 });
