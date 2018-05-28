c5da4c6b9ea692f8f8f4fca500277471f4fc5eb6
nona12 <nuriahervella@gmail.com>
2018-05-28 16:41:49 +0200

    modify methods to use them with the data base

--- backend/controllers/bar.js
+++ backend/controllers/bar.js
@@ -1,32 +1,98 @@
+var Bar = require('../models/bar')
+
 function getBar(req, res) {
-    // obtenemos el id del coche que quieren obtener
+    // obtenemos el id del bar a buscar
     var barId = req.params.id
-    res.status(200).send({ accion: "get", data: barId })
-    //TODO falta acceder a la base de datos 
+    // realizamos la consulta, buscando un bar por id 
+    Bar.findById(barId, (err, barObtenido) => {
+        if (err) {
+            // si la consulta genera algun error 
+            res.status(500).send({ accion: "get one", menssage: 'error al devolver el bar' })
+        } else if (!barObtenido) {
+            // si no se ha encontrado el bar
+            res.status(404).send({ accion: "get one", menssage: 'no hay bar' })
+        } else {
+            // si todo ha ido bien 
+            res.status(200).send({ accion: "get one", data: barObtenido })
+        }
+    })
 }
 
-function getHotels(req, res) {
-    // obtenemos el id del coche que quieren obtener
-    var params = req.body
-    res.status(200).send({ accion: "get all", data: params })
-    //TODO falta acceder a la base de datos 
+
+function getBars(req, res) {
+    // obtenemos todos los bares ordenando por id
+    Bar.find({}).sort('-_id').exec((err, bars) => {
+        if (err) {
+            // si la consulta da un error
+            res.status(500).send({ accion: "get all", menssage: 'error al devolver los bares' })
+        } else if (!bars) { // si no se han obtenido ningún
+            res.status(404).send({ accion: "get all", menssage: 'no hay bares guardados' })
+        } else { // si todo ha ido bien
+            res.status(200).send({ accion: "get all", data: bars })
+        }
+    })
 }
 
 function saveBar(req, res) {
     var params = req.body
-    res.status(200).send({ accion: "save", data: params })
-    //TODO falta acceder a la base de datos 
+    // creamos un objeto del modelo Hotel
+    var bar = new Bar();
+    // rellenamos un el objeto
+    bar.name = params.name
+    bar.type = params.type
+    bar.direccion = params.direccion
+    bar.phone = params.phone
+    bar.city = params.city
+    // guardamos el favorito
+    bar.save((err, saveStored) => {
+        if (err) {
+            // si hay un error devolvemos un error de tipo servidor 500
+            res.status(500).send({ menssage: 'error al guardar el bar' })
+        } else {
+            res.status(200).send({ accion: "save", bar: barStored })
+        }
+    })
 }
 
 function updateBar(req, res) {
+    // obtenemos el id del coche a modificar
     var barId = req.params.id
+    // obtenemos los nuevo datos del coche
     var params = req.body
-    res.status(200).send({ accion: "update", idCambiar: barId, bar: params })
+    // realizamos una consulta de actualización
+    Bar.findByIdAndUpdate(barId, params, (err, barUpdated) => {
+        if (err) {
+            // si hay un error en la consulta devolvemos un error de tipo servidor 500
+            res.status(500).send({ menssage: 'error al actualizar el bar' })
+        } else { // si todo ha ido bien 
+            res.status(200).send({ accion: "update", bar: barUpdated })
+        }
+    })
 }
 
-function deletebar(req, res){
-     var barId = req.params.id
-      res.status(200).send({accion:"delete", data: barId}) 
-    } 
+function deleteBar(req, res) {
+    // obtenemos el id del bar a borrar 
+    var barId = req.params.id
+    // buscamos el bar que deseamos borrar
+    Bar.findById(barId, (err, barEncontrado) => {
+        if (err) {
+            // si hay un error en la consulta 
+            res.status(500).send({ accion: "delete", menssage: 'error al devolver el bar' })
+        } else if (!barEncontrado) {
+            // si no se ha encontrado el bar 
+            res.status(404).send({ accion: "delete", menssage: 'no existe el bar' })
+        } else {
+            // ojo usa barEncontrado y no Bar(que borra todo)
+            barEncontrado.remove((err) => {
+                if (err) {
+                    // si el bar no se ha podido borrar
+                    res.status(500).send({ menssage: 'el bar no se ha podidio borrar' })
+                } else { // si todo ha ido bien
+                    res.status(200).send({ message: "el bar se ha eliminado" })
+                }
+            })
+        }
+    })
+}
 
 module.exports = { getBar, getBars, saveBar, updateBar, deleteBar }
--- backend/controllers/hotel.js
+++ backend/controllers/hotel.js
@@ -1,32 +1,99 @@
+var Hotel = require('../models/hotel')
+
 
 function getHotel(req, res) {
-    // obtenemos el id del coche que quieren obtener
+    // obtenemos el id del hotel a buscar
     var hotelId = req.params.id
-    res.status(200).send({ accion: "get", data: hotelId })
-    //TODO falta acceder a la base de datos 
+    // realizamos la consulta, buscando un hotel por id 
+    Hotel.findById(hotelId, (err, hotelObtenido) => {
+        if (err) {
+            // si la consulta genera algun error 
+            res.status(500).send({ accion: "get one", menssage: 'error al devolver el hotel' })
+        } else if (!hotelObtenido) {
+            // si no se ha encontrado el hotel
+            res.status(404).send({ accion: "get one", menssage: 'no hay hoteles' })
+        } else {
+            // si todo ha ido bien 
+            res.status(200).send({ accion: "get one", data: hotelObtenido })
+        }
+    })
 }
 
 function getHotels(req, res) {
-    var params = req.body
-    res.status(200).send({ accion: "get all", data: params })
-    //TODO falta acceder a la base de datos 
+    // obtenemos todos los coches ordenando por id
+    Hotel.find({}).sort('-_id').exec((err, hotels) => {
+        if (err) {
+            // si la consulta da un error
+            res.status(500).send({ accion: "get all", menssage: 'error al devolver los hoteles' })
+        } else if (!hotels) { // si no se han obtenido ningún
+            res.status(404).send({ accion: "get all", menssage: 'no hay hoteles guardados' })
+        } else { // si todo ha ido bien
+            res.status(200).send({ accion: "get all", data: hotels })
+        }
+    })
 }
 
 function saveHotel(req, res) {
     var params = req.body
-    res.status(200).send({ accion: "save", data: params })
-    //TODO falta acceder a la base de datos 
+    // creamos un objeto del modelo Hotel
+    var hotel = new Hotel();
+    // rellenamos el objeto
+    hotel.name = params.name
+    hotel.type = params.type
+    hotel.direccion = params.direccion
+    hotel.phone = params.phone
+    hotel.city = params.city
+    // guardamos el favorito
+    hotel.save((err, hotelStored) => {
+        if (err) {
+            // si hay un error devolvemos un error de tipo servidor 500
+            res.status(500).send({ menssage: 'error al guardar el hotel' })
+        } else {
+            res.status(200).send({ accion: "save", hotel: hotelStored })
+        }
+    })
 }
+
 
 function updateHotel(req, res) {
-    var hotelId = req.params.id
-    var params = req.body
-    res.status(200).send({ accion: "update", idCambiar: hotelId, hotel: params })
-}
+       // obtenemos el id del hotel a modificar
+       var hotelId = req.params.id
+       // obtenemos los nuevo datos del hotel
+       var params = req.body
+       // realizamos una consulta de actualización
+       Hotel.findByIdAndUpdate(hotelId, params, (err, hotelUpdated) => {
+           if (err) {
+               // si hay un error en la consulta devolvemos un error de tipo servidor 500
+               res.status(500).send({ menssage: 'error al actualizar el hotel' })
+           } else { // si todo ha ido bien 
+               res.status(200).send({ accion: "update", hotel: hotelUpdated })
+           }
+       })
+   }
 
 function deleteHotel(req, res) {
-    var hotelId = req.params.id
-    res.status(200).send({ accion: "delete", data: hotelId })
+   // obtenemos el id del hotel a borrar 
+   var hotelId = req.params.id
+   // buscamos el hotel que deseamos borrar
+   Hotel.findById(hotelId, (err, hotelEncontrado) => {
+       if (err) {
+           // si hay un error en la consulta 
+           res.status(500).send({ accion: "delete", menssage: 'error al devolver el hotel' })
+       } else if (!hotelEncontrado) {
+           // si no se ha encontrado el hotel 
+           res.status(404).send({ accion: "delete", menssage: 'no existe el hotel' })
+       } else {
+           // ojo usa hotelEncontrado y no Hotel(que borra todo)
+           hotelEncontrado.remove((err) => {
+               if (err) {
+                   // si elhotelbar no se ha podido borrar
+                   res.status(500).send({ menssage: 'el hotel no se ha podidio borrar' })
+               } else { // si todo ha ido bien
+                   res.status(200).send({ message: "el hotel se ha eliminado" })
+               }
+           })
+       }
+   })
 }
 
 
